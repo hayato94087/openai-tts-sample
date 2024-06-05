14a7051126ebf52f204a980de12d0b1b3d829645
hayato94087 <hayato94087@gmail.com>
2024-06-06 08:54:51 +0900

    OpenAI で 日本語を試す

---
+++ openai-tts-demo02.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+
+// ファイルシステムを扱うためのモジュール
+import fs from "fs";
+
+// ファイルパスを扱うためのモジュール
+import path from "path";
+
+// OpenAI SDKをインポート
+import { OpenAI } from "openai";
+
+// OpenAI SDKのインスタンスを生成
+const openai = new OpenAI();
+
+// OpenAI APIを利用してテキストから音声を生成する
+const speechFile = path.resolve("./speech-demo02.mp3");
+
+async function main() {
+  // OpenAI APIを利用してテキストから音声を生成
+  const mp3 = await openai.audio.speech.create({
+    // モデルの指定
+    model: "tts-1",
+    // ボイスの指定
+    voice: "alloy",
+    // テキストの指定
+    input: "えーと、はじめまして、私の名前は太郎です。みんな！はーじめましてー！！はーっはっはっは",
+  });
+  // ファイルパスを表示
+  console.log(speechFile);
+  // バッファをファイルに書き込む
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  // ファイルに書き込む
+  await fs.promises.writeFile(speechFile, buffer);
+}
+
+// メイン関数を実行
+main();
